ac8525c913d5ca68a6e5163ede76d5088fb09f68
changju <busbtvi@gmail.com>
2023-09-06 17:40:42 +0900

    inint

---
+++ palindrome_10942.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/10942
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, nums[2001], dp[2001][2001];
+    cin >> n;
+    for(int i=1; i<=n; i++) cin >> nums[i];
+    cin >> m;
+
+    
+
+    for(int i=0,s,e; i<m; i++){
+        cin >> s >> e;
+    }
+    // $sudo apt install htop
+}
