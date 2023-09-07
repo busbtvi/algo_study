ff8affa38c3255456dfff81f1d506af72969b94a
changju <busbtvi@gmail.com>
2023-09-07 18:32:36 +0900

    10942 cpp correct, tring rust

--- palindrome_10942.cpp
+++ palindrome_10942.cpp
@@ -6,15 +6,22 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m, nums[2001], dp[2001][2001];
+    int n, m, nums[2001];
+    bool dp[2001][2001];
     cin >> n;
     for(int i=1; i<=n; i++) cin >> nums[i];
     cin >> m;
 
-    
+    for(int r=1; r<=n; r++){
+        for(int c=1; c<=r; c++){
+            if(r == c) dp[r][c] = true;
+            else if(r-1 == c) dp[r][c] = (nums[r]==nums[c]);
+            else dp[r][c] = (dp[r-1][c+1] && nums[r]==nums[c]);
+        }
+    }    
 
     for(int i=0,s,e; i<m; i++){
         cin >> s >> e;
+        cout << dp[e][s] << '\n';
     }
-    // $sudo apt install htop
 }
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,42 @@
+use std::io::{self, Read};
+// use scanf::scanf;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    // // match io::stdin().read_line(&mut buffer)
+    // // {
+    // //     Ok(n) => {
+    // //         println!("{} bytes read", n);
+    // //         println!("{}", buffer);
+    // //     }
+    // //     Err(error) => println!("error: {}", error),
+    // // }
+
+    // let stdin = io::stdin()
+    //     .read_line(&mut buffer)
+    //     .expect("error");
+    // println!("hello, {}", buffer);
+
+    // println!("Price of {} is {:.2}", integer, float);
+
+    let mut n = 0;
+    // let mut m = 0;
+
+    // let mut nums: [i32; 2001] = [0; 2001];
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("read n fail");
+    n = buffer.trim().parse().expect("parsing fail");
+
+    let mut nums = vec![0; n];
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("read numbers failed");
+    let nums = buffer.trim()
+        .split_whitespace()
+        .map(|x| x.parse().expect("parse error"));
+    
+    for i in 1..n+1 {
+        // io::stdin().
+    }
 }
