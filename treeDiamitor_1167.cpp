bca8c497a9f8092ae35c101446b60c661ad95099
ChangJu <busbtvi@gmail.com>
2024-01-16 01:42:05 +0900

    lldb, c++ extension configure, Makefile for cleaning

---
+++ treeDiamitor_1167.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/1167
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int v;
+    cin >> v;
+
+}
