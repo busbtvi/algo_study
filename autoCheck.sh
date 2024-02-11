26ab593583bd20ee69d2819cda4d8536306ac0d9
ChangJu <busbtvi@gmail.com>
2024-02-11 20:54:02 +0900

    update autoCheck remove progress, add color

--- autoCheck.sh
+++ autoCheck.sh
@@ -36,20 +36,21 @@ Url=${Url:3}
 # https://www.acmicpc.net/problem/15686
 # -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 
-SampleData=`curl -A "Mozilla/5.0" $Url | sed -n '/<pre class=\"sampledata\"/,/<\/pre>/p'`
+# get HTML source, and extract sample data wrapped in <pre class="sampledata"></pre> tag
+SampleData=`curl -s -A "Mozilla/5.0" $Url | sed -n '/<pre class=\"sampledata\"/,/<\/pre>/p'`
 # https://askubuntu.com/questions/962551/extract-the-content-from-a-file-between-two-match-patterns-extract-only-html-fr
 # SampleData=`sed -n '/<pre class=\"sampledata\"/,/<\/pre>/p' ./result.html`
 # https://stackoverflow.com/questions/13242469/how-to-use-sed-grep-to-extract-text-between-two-words
 # SampleData=`sed -e 's/Here\(.*\)String/\1/'`
 # SampleData=`sed -e 's/<pre class=\"sampledata\"\(.*\)<\/pre>/\1/' ./result.html`
 # https://mywiki.wooledge.org/BashFAQ/073
-# echo $SampleData
 
 PRE_IFS=$IFS # 본래 IFS값을 백업해논다.
 IFS=$'\n'
 
 CurrentMode="input"
 CurrentTest=1
+AllTestSuccess=true
 touch ./sample.input
 touch ./sample.output
 
@@ -65,11 +66,14 @@ for word in $SampleData; do
         if [ $CurrentMode == "output" ]; then
             Result=`timeout 3s ./$TargetExe < sample.input`
             if [ $? -eq 124 ]; then 
-                echo "$CurrentTest: Timeout"
+                printf "\e[93m $CurrentTest: Timeout\n\e[0m"
+                AllTestSuccess=false
             else 
                 if [ -z `diff -w ./sample.output - <<< $Result` ]; then
-                    echo "$CurrentTest: ✅"
-                else echo "$CurrentTest: ❌"
+                    printf "\e[92m $CurrentTest: pass\n\e[0m"
+                else
+                    printf "\e[91m $CurrentTest: fail\n\e[0m"
+                    AllTestSuccess=false
                 fi
             fi
             ((CurrentTest++))
@@ -77,6 +81,12 @@ for word in $SampleData; do
     else echo $word >> ./sample.$CurrentMode
     fi
 done
+
+if [ $AllTestSuccess == true ]; then
+    printf "\nAll tests passed\n"
+else
+    printf "\n\e[91mSome tests failed\e[0m\n"
+fi
 IFS=$PRE_IFS # IFS 원상 복구
 
 # rm ./target.out
