d96c80952d659e077486a81d872226a8a856ef75
ChangJu <busbtvi@gmail.com>
2024-02-14 07:28:09 +0900

    tsp done

--- tsp_2098.cpp
+++ tsp_2098.cpp
@@ -1,28 +1,30 @@
 // https://www.acmicpc.net/problem/2098
 #include <iostream>
-#include <vector>
 #include <map>
+#define INTMAX 100000000
 using namespace std;
 
-int permutation[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-int costs[16][16], n, ans = ~(1<<31);
-void dfsSearch(int prevI, int prevCost){
-    if(prevI == n-2 && costs[permutation[n-2]][permutation[n-1]] && costs[permutation[n-1]][0]){
-        // back to start
-        if(ans > prevCost + costs[permutation[n-2]][permutation[n-1]] + costs[permutation[n-1]][0]){
-            ans = prevCost + costs[permutation[n-2]][permutation[n-1]] + costs[permutation[n-1]][0];
-        }
-        return;
-    }
-    if(prevCost > ans) return;
+int costs[16][16], n;
+map<int, map<int,int>> dp;  // dp[방문 안한 도시들][현재 위치] = minCost(남은도시->출발지로)
+int dfsSearch(int prevI, int notVisited){
+    if(notVisited == 0) return costs[prevI][0] ? costs[prevI][0] : INTMAX;
 
-    for(int i=prevI+1; i<n; i++){
-        if(costs[permutation[prevI]][permutation[i]]){
-            swap(permutation[prevI+1], permutation[i]);
-            dfsSearch(prevI+1, prevCost + costs[permutation[prevI]][permutation[prevI+1]]);
-            swap(permutation[prevI+1], permutation[i]);
+    int localMin = INTMAX;
+    for(int curI=1; curI<n; curI++){
+        if(((notVisited >> curI) & 1) && costs[prevI][curI]){
+            notVisited &= ~(1 << curI);
+
+            if(dp[notVisited].find(curI) == dp[notVisited].end())
+                dp[notVisited][curI] = dfsSearch(curI, notVisited);
+
+            if(localMin > costs[prevI][curI] + dp[notVisited][curI])
+                localMin = costs[prevI][curI] + dp[notVisited][curI];
+            
+            notVisited |= (1 << curI);
         }
     }
+    dp[notVisited][prevI] = localMin;
+    return localMin;
 }
 
 int main(){
@@ -32,8 +34,7 @@ int main(){
         for(int j=0; j<n; j++)
             cin >> costs[i][j];
 
-    dfsSearch(0, 0);
-    cout << ans;
+    cout << dfsSearch(0, (0xFFFF >> (17-n)) << 1);
 
     return 0;
 }
