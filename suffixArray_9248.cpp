9311c9836bc64d12f17a69a8df32d8b7a614734e
ChangJu <busbtvi@gmail.com>
2024-02-19 21:25:28 +0900

    suffix Array timeout(giveup)

--- suffixArray_9248.cpp
+++ suffixArray_9248.cpp
@@ -5,54 +5,53 @@
 using namespace std;
 
 int main(){
-	int lookBack = 0;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int lookBack = 0, sLen;
 	string s;
 	cin >> s;
+	sLen = s.length();
 
 	// suffix
-	vector<int> suffix(s.size()), rank(s.size(), 0);
-	for(int i=0; i<s.size(); i++){
+	vector<int> suffix(sLen), rank(sLen, 0), newRank(sLen, 0);
+	for(int i=0; i<sLen; i++){
 		suffix[i] = i;
 		rank[i] = s[i];
 	}
 
-	while(lookBack < s.size()){
-		sort(suffix.begin(), suffix.end(), [&rank, &s, lookBack](const int& a, const int& b){
+	while(lookBack < sLen){
+		sort(suffix.begin(), suffix.end(), [&rank, sLen, lookBack](const int& a, const int& b){
 			if(rank[a] != rank[b]) return rank[a] < rank[b];
-			if(a+lookBack >= s.length() || b+lookBack >= s.length()) return a > b;
+			if(a+lookBack >= sLen || b+lookBack >= sLen) return a > b;
 			return rank[a+lookBack] < rank[b+lookBack];
 		});
 
-		vector<int> newRank(s.size(), 0);
-		for(int i=1, j,k; i<s.size(); i++){
-			// if(rank[suffix[i-1]] != rank[suffix[i]] || rank[suffix[i-1]+lookBack] != rank[suffix[i]+lookBack]){
-			// 	newRank[suffix[i]] = newRank[suffix[i-1]] + 1;
-			// }
-			// else newRank[suffix[i]] = newRank[suffix[i-1]];
+		newRank[0] = 0;
+		for(int i=1, j,k; i<sLen; i++){
 			j = suffix[i-1];
 			k = suffix[i];
-			if(rank[j]==rank[k] && (k+lookBack)<s.size() && (j+lookBack)<s.size() && rank[k+lookBack]==rank[j+lookBack])
+			if(rank[j]==rank[k] && (k+lookBack)<sLen && (j+lookBack)<sLen && rank[k+lookBack]==rank[j+lookBack])
 				newRank[k] = newRank[j];
 			else newRank[k] = newRank[j] + 1;
 		}
-		for(int i=0; i<s.size(); i++) rank[i] = newRank[i];
+		
+		for(int i=0; i<sLen; i++) rank[i] = newRank[i];
 
 		lookBack = (lookBack == 0) ? 1 : lookBack*2;
 	}
 
 	// lcp
-	vector<int> lcp(s.size());
-	for(int i=0, j, c; i<s.length(); i++){
+	vector<int> lcp(sLen);
+	for(int i=0, j, c; i<sLen; i++){
 		c = 0;
 		j = suffix[rank[i]-1];
-		while(j+c<s.size() && s[i+c]==s[j+c]) c++;
+		while(j+c<sLen && s[i+c]==s[j+c]) c++;
 		
 		lcp[rank[i]] = c;
 	}
 
 	for(auto c: suffix) cout << c+1 << ' ';
 	cout << "\nx ";
-	for(int i=1; i<lcp.size(); i++) cout << lcp[i] << ' ';
+	for(int i=1; i<sLen; i++) cout << lcp[i] << ' ';
 
 	return 0;
 }
